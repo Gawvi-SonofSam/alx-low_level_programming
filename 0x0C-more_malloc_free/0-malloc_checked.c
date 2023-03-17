54880efc92e178cad7fb56c05d17a8e49146b690
Gawvi-SonofSam <ayaribagabriel@gmail.com>
2023-03-16 18:41:49 -0700

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,11 @@
 #include "main.h"
 		
 
-		
-/**
+/*
 		
  * *malloc_checked - allocates memory using malloc
 		
  * @b: number of bytes to allocate
-		
- *
 		
  * Return: a pointer to the allocated memory
 		
